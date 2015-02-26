f1842c49825ca4f09c5ed4ed1e8923baa99646ce
Balaji Mohan <balaj@raremile.com>
2015-02-26 19:16:39 +0530

    Completed LoanDaoImpl

--- NewFiWebApp/NewfiCommon/src/main/java/com/nexera/common/dao/impl/LoanDaoImpl.java
+++ NewFiWebApp/NewfiCommon/src/main/java/com/nexera/common/dao/impl/LoanDaoImpl.java
@@ -1,9 +1,12 @@
 package com.nexera.common.dao.impl;
 
+import java.util.ArrayList;
+import java.util.Collections;
 import java.util.Date;
 import java.util.List;
 
 import org.hibernate.Criteria;
+import org.hibernate.Hibernate;
 import org.hibernate.Session;
 import org.hibernate.criterion.Restrictions;
 import org.springframework.stereotype.Component;
@@ -67,12 +70,44 @@ public class LoanDaoImpl extends GenericDaoImpl implements LoanDao {
 	@Override
 	public List<User> retreiveLoanTeam(Loan loan) {
 		// TODO Auto-generated method stub
-		return null;
+
+		Session session = sessionFactory.getCurrentSession();
+		Criteria criteria = session.createCriteria(LoanTeam.class);
+		criteria.add(Restrictions.eq("loan", loan));
+		List<LoanTeam> team = criteria.list();
+
+		if (team != null && team.size() > 0) {
+			List<User> userList = new ArrayList<User>();
+			for (LoanTeam loanTeam : team) {
+				User user = loanTeam.getUser();
+				if (user != null)
+					Hibernate.initialize(user.getUserRole());
+				userList.add(user);
+			}
+
+			return userList;
+		}
+		return Collections.EMPTY_LIST;
 	}
 
 	@Override
 	public List<Loan> retreiveLoansAsManager(User loanManager) {
-		// TODO Auto-generated method stub
-		return null;
+
+		Session session = sessionFactory.getCurrentSession();
+		Criteria criteria = session.createCriteria(LoanTeam.class);
+		criteria.add(Restrictions.eq("user", loanManager));
+		List<LoanTeam> team = criteria.list();
+
+		if (team != null && team.size() > 0) {
+			List<Loan> loanList = new ArrayList<Loan>();
+			for (LoanTeam loanTeam : team) {
+				Loan loan = loanTeam.getLoan();
+				loanList.add(loan);
+			}
+			return loanList;
+
+		}
+
+		return Collections.EMPTY_LIST;
 	}
 }
