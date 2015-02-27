d6a1f09d3406f6ab3e09fda94c7fb5cd14b8ff42
Anoop <anoop@raremile.com>
2015-02-27 17:39:42 +0530

    Added VO related to loan dashboard vo

---
+++ NewFiWebApp/NewfiCommon/src/main/java/com/nexera/common/vo/NotesVO.java
@@ -0,0 +1,29 @@
+package com.nexera.common.vo;
+
+public class NotesVO {
+
+	private String name;
+	private String time;
+	private String message;
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public String getTime() {
+		return time;
+	}
+	public void setTime(String time) {
+		this.time = time;
+	}
+	public String getMessage() {
+		return message;
+	}
+	public void setMessage(String message) {
+		this.message = message;
+	}
+	
+	
+	
+}
