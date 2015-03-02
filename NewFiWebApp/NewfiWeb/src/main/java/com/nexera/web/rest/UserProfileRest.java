2e91d66d3409d35c54117367a2dc25ae26a41273
Balaji Mohan <balaj@raremile.com>
2015-03-02 14:13:10 +0530

    Started dev of create user rest Service.

--- NewFiWebApp/NewfiWeb/src/main/java/com/nexera/web/rest/UserProfileRest.java
+++ NewFiWebApp/NewfiWeb/src/main/java/com/nexera/web/rest/UserProfileRest.java
@@ -8,7 +8,6 @@ import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -109,5 +108,12 @@ public class UserProfileRest {
 		CommonResponseVO responseVO = RestUtil.wrapObjectForSuccess(userList);
 
 		return new Gson().toJson(responseVO);
+	}
+	@RequestMapping(method = RequestMethod.POST)
+	public @ResponseBody String  createUser(){
+		
+		
+		return "";
+		
 	}
 }
