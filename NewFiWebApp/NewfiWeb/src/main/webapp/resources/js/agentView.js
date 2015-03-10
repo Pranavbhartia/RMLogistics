6da595ce74ccdf55b6911de68a8814f3a96d9833
charujoshi <charuindia12@gmail.com>
2015-03-10 20:22:25 +0530

    Add notification Bug fix

--- NewFiWebApp/NewfiWeb/src/main/webapp/resources/js/agentView.js
+++ NewFiWebApp/NewfiWeb/src/main/webapp/resources/js/agentView.js
@@ -603,7 +603,7 @@ function appendSchedulerContainer(contxt) {
 		pickDate : false
 	});
 }
-function getSchedulerContainer(contxt,data){
+function getSchedulerContainer(contxt){
 	var wrapper = $('<div>').attr({
 		"class" : "cust-detail-rw float-left"
 	});
@@ -665,15 +665,18 @@ function getSchedulerContainer(contxt,data){
 
 	var col1Btn = $('<div>').attr({
 		"class" : "msg-btn-submit float-right"
-	}).html("Submit").bind("click",{contxt:contxt,data:data},function(e){
+	}).html("Submit").bind("click",{contxt:contxt},function(e){
 		var dat=$('#sch-msg-time-picker ').data('DateTimePicker').getDate()._d	
 		var snoozeTime=$('#sch-msg-date-picker').data('datepicker').getDate();
 		snoozeTime.setHours(dat.getHours());
 		snoozeTime.setMinutes(dat.getMinutes())
 		var message=$("#sch-msg-message").val();
-		if(snoozeTime!="Invalid Date"){
-			if(data===undefined)
-				data={};
+		if(message==""){
+			showToastMessage("Invalid Message");
+		}else if(snoozeTime=="Invalid Date"){
+			showToastMessage("Invalid Date");
+		}else{
+			var data={};
 			data.content=message;
 			data.createdDate=new Date().getTime();
 			data.remindOn=snoozeTime.getTime();
