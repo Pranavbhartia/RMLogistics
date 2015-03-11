5172f0b2b57dec7c0030a804d15e476e0e0f132d
rajeswarid <rajeswari@raremile.com>
2015-03-11 15:12:25 +0530

    Signature changes for Task Executors

--- NewFiWebApp/NewfiWorkFlow/src/main/java/com/nexera/newfi/workflow/WorkflowDisplayConstants.java
+++ NewFiWebApp/NewfiWorkFlow/src/main/java/com/nexera/newfi/workflow/WorkflowDisplayConstants.java
@@ -3,4 +3,8 @@ package com.nexera.newfi.workflow;
 public class WorkflowDisplayConstants {
 
 	public static final String ALERT_MANAGER_TEXT = "Schedule an Alert";
+	
+	public static final String LOAN_PROGRESS_TEXT = "Click here to complete Application form";
+	
+	public static final String LOAN_PROGRESS_LQB = "LQB -";
 }
--- NewFiWebApp/NewfiWorkFlow/src/main/java/com/nexera/newfi/workflow/tasks/EMailSender.java
+++ NewFiWebApp/NewfiWorkFlow/src/main/java/com/nexera/newfi/workflow/tasks/EMailSender.java
@@ -1,27 +1,31 @@
 package com.nexera.newfi.workflow.tasks;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 import com.nexera.workflow.task.IWorkflowTaskExecutor;
 
 public class EMailSender implements IWorkflowTaskExecutor {
+	private static final Logger LOG = LoggerFactory
+	        .getLogger(EMailSender.class);
 
-	public String execute(Object... objects) {
+	public String execute(Object[] objects) {
 		// Call the Email Sender here.
 		if (objects != null) {
 			String emailTemplate = objects[0].toString();
+			LOG.debug("Template name is " + emailTemplate);
 		}
-		return null;
+		return "";
 	}
-
-	
 
 	public Object[] getParamsForExecute() {
 		// TODO Auto-generated method stub
 		return null;
 	}
 
-	public Object renderStateInfo(Object... inputs) {
-	    // TODO Auto-generated method stub
-	    return null;
-    }
+	public String renderStateInfo(String[] inputs) {
+
+		return "";
+	}
 
 }
