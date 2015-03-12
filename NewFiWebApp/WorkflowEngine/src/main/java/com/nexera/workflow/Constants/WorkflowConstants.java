db2b506162f597027958b48480a8a43201ee96d2
utsav09211 <utsav.sharma@raremile.com>
2015-03-12 11:14:04 +0530

    Workflow Changes - Utsav

--- NewFiWebApp/WorkflowEngine/src/main/java/com/nexera/workflow/Constants/WorkflowConstants.java
+++ NewFiWebApp/WorkflowEngine/src/main/java/com/nexera/workflow/Constants/WorkflowConstants.java
@@ -7,22 +7,22 @@ package com.nexera.workflow.Constants;
  * @author Utsav
  *
  */
-public class WorkflowConstants
-{
-    public static final String PROPERTY_FILE_NAME = "application";
+public class WorkflowConstants {
+	public static final String PROPERTY_FILE_NAME = "application";
 
-    public static final String POOL_SIZE = "pool.size";
+	public static final String POOL_SIZE = "pool.size";
 
-    public static final String SUCCESS = "success";
+	public static final String SUCCESS = "success";
 
-    public static final String FAILURE = "failure";
+	public static final String FAILURE = "failure";
 
-    public static final String PENDING = "pending";
+	public static final String PENDING = "pending";
 
-    public static final String RENDER_STATE_INFO_METHOD = "renderStateInfo";
+	public static final String RENDER_STATE_INFO_METHOD = "renderStateInfo";
 
-    public static final String EXECUTE_METHOD = "execute";
-    
-    public static final String LOAN_MANAGER_WORKFLOW_TYPE = "";
-    public static final String CUSTOMER_WORKFLOW_TYPE = "";
+	public static final String EXECUTE_METHOD = "execute";
+
+	public static final String LOAN_MANAGER_WORKFLOW_TYPE = "LM_WF_ALL";
+
+	public static final String CUSTOMER_WORKFLOW_TYPE = "CUST_WF_ALL";
 }
