3c66da603c8a55dd3e0023c2a546bd91c192eb72
Balaji Mohan <balaj@raremile.com>
2015-03-13 18:06:40 +0530

    Workflow related changes Changed fetch type to eager for master tables in workflow entities Made the add to team workflow service work after changes

---
+++ NewFiWebApp/NewfiCommon/src/main/java/com/nexera/common/vo/MilestoneLoanTeamVO.java
@@ -0,0 +1,27 @@
+package com.nexera.common.vo;
+
+public class MilestoneLoanTeamVO {
+	private Integer milestoneID;
+	private Integer loanID;
+	private Integer userID;
+	public Integer getMilestoneID() {
+		return milestoneID;
+	}
+	public void setMilestoneID(Integer milestoneID) {
+		this.milestoneID = milestoneID;
+	}
+	public Integer getLoanID() {
+		return loanID;
+	}
+	public void setLoanID(Integer loanID) {
+		this.loanID = loanID;
+	}
+	public Integer getUserID() {
+		return userID;
+	}
+	public void setUserID(Integer userID) {
+		this.userID = userID;
+	}
+	
+	
+}
