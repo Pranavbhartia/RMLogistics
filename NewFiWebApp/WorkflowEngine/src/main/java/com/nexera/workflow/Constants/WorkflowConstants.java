b714ac4a77fd6e0ac56aa240bb275b149eb32107
rajeswarid <rajeswari@raremile.com>
2015-03-17 10:01:37 +0530

    Added attribute workflow Type

--- NewFiWebApp/WorkflowEngine/src/main/java/com/nexera/workflow/Constants/WorkflowConstants.java
+++ NewFiWebApp/WorkflowEngine/src/main/java/com/nexera/workflow/Constants/WorkflowConstants.java
@@ -4,8 +4,7 @@
 package com.nexera.workflow.Constants;
 
 /**
- * @author Utsav
- *T
+ * @author Utsav T
  */
 public class WorkflowConstants {
 	public static final String PROPERTY_FILE_NAME = "application";
@@ -25,4 +24,32 @@ public class WorkflowConstants {
 	public static final String LOAN_MANAGER_WORKFLOW_TYPE = "LM_WF_ALL";
 
 	public static final String CUSTOMER_WORKFLOW_TYPE = "CUST_WF_ALL";
+
+	public static final String WORKFLOW_ITEM_INITIAL_CONTACT = "INITIAL_CONTACT";
+
+	public static final String WORKFLOW_ITEM_UW_STATUS = "UW_STATUS";
+
+	public static final String WORKFLOW_ITEM_TEAM_STATUS = "TEAM_STATUS";
+	public static final String WORKFLOW_ITEM_SYSTEM_EDU = "SYSTEM_EDU";
+	public static final String WORKFLOW_ITEM_RATES_EDU = "RATES_EDU";
+	public static final String WORKFLOW_ITEM_QC_STATUS = "QC_STATUS";
+	public static final String WORKFLOW_ITEM_PROFILE_INFO = "PROFILE_INFO";
+	public static final String WORKFLOW_ITEM_NEEDS_STATUS = "NEEDS_STATUS";
+	public static final String WORKFLOW_ITEM_NEEDS_EDU = "NEEDS_EDU";
+
+	public static final String WORKFLOW_ITEM_LOCK_RATE = "LOCK_RATE";
+	public static final String WORKFLOW_ITEM_LOAN_PROGRESS = "LOAN_PROGRESS";
+	public static final String WORKFLOW_ITEM_DISCLOSURE_STATUS = "DISCLOSURE_STATUS";
+
+	public static final String WORKFLOW_ITEM_CREDIT_SCORE = "CREDIT_SCORE";
+	public static final String WORKFLOW_ITEM_CREDIT_BUREAU = "CREDIT_BUREAU";
+	public static final String WORKFLOW_ITEM_COMM_EDU = "COMM_EDU";
+	public static final String WORKFLOW_ITEM_CLOSURE_STATUS = "CLOSURE_STATUS";
+	public static final String WORKFLOW_ITEM_AUS_STATUS = "AUS_STATUS";
+	public static final String WORKFLOW_ITEM_APPRAISAL_STATUS = "APPRAISAL_STATUS";
+	public static final String WORKFLOW_ITEM_APP_FEE = "APP_FEE";
+	public static final String WORKFLOW_ITEM_APP_EDU = "APP_EDU";
+
+	public static final String WORKFLOW_ITEM_1003_COMPLETE = "1003_COMPLETE";
+
 }
