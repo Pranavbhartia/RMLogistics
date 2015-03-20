e7e71feb03a0714fdda263d61d86abb16ae2ad54
rajeswarid <rajeswari@raremile.com>
2015-03-20 15:02:47 +0530

    Workflow task Concrete Classes
    Workflow task Concrete Classes

---
+++ NewFiWebApp/NewfiWorkFlow/src/main/java/com/nexera/newfi/workflow/customer/tasks/ProfileManager.java
@@ -0,0 +1,41 @@
+package com.nexera.newfi.workflow.customer.tasks;
+
+import java.util.HashMap;
+
+import org.springframework.beans.factory.annotation.Autowired;
+
+import com.nexera.common.vo.UserVO;
+import com.nexera.core.service.UserProfileService;
+import com.nexera.newfi.workflow.WorkflowDisplayConstants;
+import com.nexera.workflow.task.IWorkflowTaskExecutor;
+
+public class ProfileManager implements IWorkflowTaskExecutor {
+	@Autowired
+	private UserProfileService userProfileService;
+	@Override
+	public String execute(HashMap<String, Object> objectMap) {
+		//This Task doesnt do anything - 
+		//Only will update the status of the work item to success.
+		return  WorkflowDisplayConstants.WORKFLOW_TASK_SUCCESS;
+	}
+
+	@Override
+	public String renderStateInfo(HashMap<String, Object> inputMap) {
+		Integer completionStatus = 0;
+		int userID = Integer.parseInt(inputMap
+		        .get(WorkflowDisplayConstants.USER_ID_KEY_NAME).toString());		
+		UserVO userVO =  userProfileService.findUser(userID);
+		if (userVO.getCustomerDetail()!= null)
+		{
+		 completionStatus = userVO.getCustomerDetail().getProfileCompletionStatus();
+		}		
+		return completionStatus.toString();
+	}
+
+	@Override
+	public String checkStatus(HashMap<String, Object> inputMap) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
--- NewFiWebApp/NewfiWorkFlow/src/main/java/com/nexera/newfi/workflow/tasks/NeededItemsManager.java
+++ NewFiWebApp/NewfiWorkFlow/src/main/java/com/nexera/newfi/workflow/tasks/NeededItemsManager.java
@@ -32,13 +32,7 @@ public class NeededItemsManager implements IWorkflowTaskExecutor {
 		StringBuffer strBuff = new StringBuffer();
 		strBuff.append(neededItemScoreVO.getTotalSubmittedItem() + " out of "
 		        + neededItemScoreVO.getNeededItemRequired());
-		
 		return new Gson().toJson(neededItemScoreVO);
-	}
-
-	public Object[] getParamsForExecute() {
-		// TODO Auto-generated method stub
-		return null;
 	}
 
 	public String checkStatus(HashMap<String, Object> inputMap) {
