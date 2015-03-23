e6fc18e5d536292fce0f2fe174126f5b58f7b4c1
charujoshi <charuindia12@gmail.com>
2015-03-23 10:33:07 +0530

    LoanAppFormDao findByuserID API

--- NewFiWebApp/NewfiCommon/src/main/java/com/nexera/common/dao/impl/LoanAppFormDaoImpl.java
+++ NewFiWebApp/NewfiCommon/src/main/java/com/nexera/common/dao/impl/LoanAppFormDaoImpl.java
@@ -1,9 +1,14 @@
 package com.nexera.common.dao.impl;
 
+import org.hibernate.Criteria;
+import org.hibernate.Session;
+import org.hibernate.criterion.Restrictions;
 import org.springframework.stereotype.Component;
 
 import com.nexera.common.dao.LoanAppFormDao;
 import com.nexera.common.entity.LoanAppForm;
+import com.nexera.common.entity.LoanNeedsList;
+import com.nexera.common.entity.User;
 
 @Component
 public class LoanAppFormDaoImpl extends GenericDaoImpl implements
@@ -14,7 +19,15 @@ public class LoanAppFormDaoImpl extends GenericDaoImpl implements
 		// TODO Auto-generated method stub
 		return null;
 	}
-	
+	public LoanAppForm findByuserID(int userID) {
+		Session session = sessionFactory.getCurrentSession();
+		Criteria criteria = session.createCriteria(LoanAppForm.class);
+		User user=new User();
+		user.setId(userID);
+		criteria.add(Restrictions.eq("user", user));
+		LoanAppForm loalAppForm=(LoanAppForm) criteria.uniqueResult();
+		return loalAppForm;
+	}
 	
 	
 
--- NewFiWebApp/NewfiWorkFlow/src/main/java/com/nexera/newfi/workflow/customer/tasks/ApplicationFormStatusManager.java
+++ NewFiWebApp/NewfiWorkFlow/src/main/java/com/nexera/newfi/workflow/customer/tasks/ApplicationFormStatusManager.java
@@ -6,6 +6,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
 import com.nexera.common.dao.LoanAppFormDao;
+import com.nexera.common.entity.LoanAppForm;
 import com.nexera.workflow.engine.EngineTrigger;
 import com.nexera.workflow.task.IWorkflowTaskExecutor;
 
@@ -14,6 +15,9 @@ public class ApplicationFormStatusManager implements IWorkflowTaskExecutor {
 
 	@Autowired
 	private EngineTrigger engineTrigger;
+	
+	@Autowired
+	private LoanAppFormDao loanAppFormDao;
 
 	@Override
 	public String execute(HashMap<String, Object> objectMap) {
@@ -31,7 +35,7 @@ public class ApplicationFormStatusManager implements IWorkflowTaskExecutor {
 	public String checkStatus(HashMap<String, Object> inputMap) {
 		// TODO Auto-generated method stub
 		int userId = Integer.parseInt(inputMap.get("userId").toString());
-		// LoanAppForm loanAppForm=loanAppFormDao.findByuserID(userId);
+		LoanAppForm loanAppForm=loanAppFormDao.findByuserID(userId);
 		/*
 		 * if(loanAppForm.isCompleted){ int
 		 * workflowItemExecId=Integer.parseInt(inputMap
