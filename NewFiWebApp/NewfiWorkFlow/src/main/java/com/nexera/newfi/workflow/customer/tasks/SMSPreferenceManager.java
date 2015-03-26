4abaa4d920af9f61e2ff99c6f4b0876a4d09026a
rajeswarid <rajeswari@raremile.com>
2015-03-26 23:12:46 +0530

    SMS Manager

---
+++ NewFiWebApp/NewfiWorkFlow/src/main/java/com/nexera/newfi/workflow/customer/tasks/SMSPreferenceManager.java
@@ -0,0 +1,33 @@
+package com.nexera.newfi.workflow.customer.tasks;
+
+import java.util.HashMap;
+
+import com.nexera.workflow.task.IWorkflowTaskExecutor;
+
+public class SMSPreferenceManager  implements IWorkflowTaskExecutor {
+
+	@Override
+    public String execute(HashMap<String, Object> objectMap) {
+	    // TODO Auto-generated method stub
+	    return null;
+    }
+
+	@Override
+    public String renderStateInfo(HashMap<String, Object> inputMap) {
+	    // TODO Auto-generated method stub
+	    return null;
+    }
+
+	@Override
+    public String checkStatus(HashMap<String, Object> inputMap) {
+	    // TODO Auto-generated method stub
+	    return null;
+    }
+
+	@Override
+    public String invokeAction(HashMap<String, Object> inputMap) {
+	    // TODO Auto-generated method stub
+	    return null;
+    }
+
+}
