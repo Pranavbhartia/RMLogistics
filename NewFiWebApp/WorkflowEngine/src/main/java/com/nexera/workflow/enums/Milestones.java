d86e36a2a1dfe567a4df1938ad685964b35cf18f
rajeswarid <rajeswari@raremile.com>
2015-03-27 10:25:16 +0530

    Milestones ENUM

--- NewFiWebApp/WorkflowEngine/src/main/java/com/nexera/workflow/enums/Milestones.java
+++ NewFiWebApp/WorkflowEngine/src/main/java/com/nexera/workflow/enums/Milestones.java
@@ -4,16 +4,34 @@ public enum Milestones {
 	App1003(1), AUSUW(2), QC(3), LM_DECISION(4), DISCLOSURE(5), APPRAISAL(6), UW(
 	        7), LOAN_CLOSURE(8);
 
-	private final int milestoneID;
+	private int milestoneID;
+
+	private String milestoneKey;
+
+	public String getMilestoneKey() {
+		return milestoneKey;
+	}
 
 	private Milestones(int milestoneId) {
 		milestoneID = milestoneId;
 	}
 
-	public Milestones getMileStone(int statusValue) {
+	public Milestones getMileStone(int inputID) {
 		Milestones mileStone = Milestones.App1003;
 		for (Milestones ms : Milestones.values()) {
-			if (ms.milestoneID == statusValue) {
+			if (ms.milestoneID == inputID) {
+				mileStone = ms;
+				break;
+			}
+		}
+		return mileStone;
+	}
+
+	public Milestones getMileStone(String milestoneKey) {
+		Milestones mileStone = Milestones.App1003;
+		for (Milestones ms : Milestones.values()) {
+			if (milestoneKey != null
+			        && milestoneKey.equalsIgnoreCase(ms.getMilestoneKey())) {
 				mileStone = ms;
 				break;
 			}
