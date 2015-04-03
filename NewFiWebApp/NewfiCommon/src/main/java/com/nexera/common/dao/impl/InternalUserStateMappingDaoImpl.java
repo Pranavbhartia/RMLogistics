24581029ccb7f693afdd4d5692314c83ae87c153
Lavanyagowda01 <lavanya@raremile.com>
2015-04-03 19:54:31 +0800

    Loan manager profile to associate state mapping

--- NewFiWebApp/NewfiCommon/src/main/java/com/nexera/common/dao/impl/InternalUserStateMappingDaoImpl.java
+++ NewFiWebApp/NewfiCommon/src/main/java/com/nexera/common/dao/impl/InternalUserStateMappingDaoImpl.java
@@ -1,11 +1,43 @@
 package com.nexera.common.dao.impl;
 
+import java.util.List;
+
+import org.hibernate.Criteria;
+import org.hibernate.Session;
+import org.hibernate.criterion.Restrictions;
 import org.springframework.stereotype.Component;
 import org.springframework.transaction.annotation.Transactional;
+
 import com.nexera.common.dao.InternalUserStateMappingDao;
+import com.nexera.common.entity.InternalUserStateMapping;
+import com.nexera.common.entity.MileStoneTurnAroundTime;
 
 @Component
 @Transactional
 public class InternalUserStateMappingDaoImpl extends GenericDaoImpl implements InternalUserStateMappingDao {
+
+	@Override
+	public void saveOrUpdateUserStates(
+			List<InternalUserStateMapping> internalUserStateMappings) {
+			for (InternalUserStateMapping internalUserStateMapping : internalUserStateMappings) {
+				this.saveOrUpdate(internalUserStateMapping);
+			}
+	}
+
+	@Override
+	public List<InternalUserStateMapping> retrieveStatesForUser(Integer userId) {
+		Session session = sessionFactory.getCurrentSession();
+		Criteria criteria = session
+				.createCriteria(InternalUserStateMapping.class);
+		criteria.add(Restrictions.eq("user.id",
+				userId));
+		return criteria.list();
+	}
+
+	@Override
+	public void deleteObj(InternalUserStateMapping stateMapping) {
+		Session session = sessionFactory.getCurrentSession();
+		session.delete(stateMapping);
+	}
 
 }
