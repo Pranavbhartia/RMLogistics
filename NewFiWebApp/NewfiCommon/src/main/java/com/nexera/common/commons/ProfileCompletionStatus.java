a402769400092e5299c5276f002013142dd60122
Ranjitha.R <ranjitha.r@raremile.com>
2015-04-05 21:13:59 +0530

    BUG FIX-User Profile Status

--- NewFiWebApp/NewfiCommon/src/main/java/com/nexera/common/commons/ProfileCompletionStatus.java
+++ NewFiWebApp/NewfiCommon/src/main/java/com/nexera/common/commons/ProfileCompletionStatus.java
@@ -2,8 +2,9 @@ package com.nexera.common.commons;
 
 public interface ProfileCompletionStatus {
 
-	public static final Integer ON_CREATE = 33;
-	public static final Integer ON_PROFILE_PIC_UPLOAD = 67;
+	public static final Integer ON_CREATE = 34;
+	public static final Integer ON_PROFILE_PIC_UPLOAD = 66;
 	public static final Integer ON_PROFILE_COMPLETE = 100;
+
 
 }
