93ee10faead5af777c5e5e92b181d9e7366e5ffe
rajeswarid <rajeswari@raremile.com>
2015-04-06 19:47:54 +0530

    Milestone Notification type Enum

--- NewFiWebApp/NewfiCommon/src/main/java/com/nexera/common/enums/MilestoneNotificationTypes.java
+++ NewFiWebApp/NewfiCommon/src/main/java/com/nexera/common/enums/MilestoneNotificationTypes.java
@@ -2,7 +2,8 @@ package com.nexera.common.enums;
 
 public enum MilestoneNotificationTypes {
 	CREDIT_SCORE_NOTIFICATION_TYPE("CREDIT_SCORE_NOTIFICATION"), SYS_EDU_NOTIFICATION_TYPE(
-	        "SYS_EDU_NOTIFICATION");
+	        "SYS_EDU_NOTIFICATION"), DISCLOSURE_AVAIL_NOTIFICATION_TYPE(
+	        "DISCLOSURE_AVAIL_NOTIFICATION");
 	MilestoneNotificationTypes(String notificationTypeName) {
 		this.notificationTypeName = notificationTypeName;
 	}
@@ -12,4 +13,18 @@ public enum MilestoneNotificationTypes {
 	}
 
 	private String notificationTypeName;
+
+	public static MilestoneNotificationTypes getNotificationTypeByName(
+	        String name) {
+		MilestoneNotificationTypes notificationType = null;
+		for (MilestoneNotificationTypes ms : MilestoneNotificationTypes
+		        .values()) {
+			if (name != null
+			        && name.equalsIgnoreCase(ms.getNotificationTypeName())) {
+				notificationType = ms;
+				break;
+			}
+		}
+		return notificationType;
+	}
 }
