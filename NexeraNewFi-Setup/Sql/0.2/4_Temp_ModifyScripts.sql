9f40d5529dcd7c22d98985670274f3de174ae92f
utsav09211 <utsav.sharma@raremile.com>
2015-04-07 16:07:32 +0530

    SQL Changes For New Columns

--- NexeraNewFi-Setup/Sql/0.2/4_Temp_ModifyScripts.sql
+++ NexeraNewFi-Setup/Sql/0.2/4_Temp_ModifyScripts.sql
@@ -6,5 +6,20 @@ UPDATE `newfi_schema`.`workflowitemmaster` SET `on_success`='28' WHERE `id`='1';
 
 #Utsav on 7 April
 ALTER TABLE `newfi_schema`.`internaluserdetails` 
-   ADD COLUMN `lqb_username` varchar(255) NULL after `user_role`, 
-   ADD COLUMN `lqb_password` varchar(255) NULL after `lqb_username`;
+   ADD COLUMN `lqb_username` varchar(255) NULL AFTER `user_role`, 
+   ADD COLUMN `lqb_password` varchar(255) NULL AFTER `lqb_username`;
+   
+#Utsav on 7th April
+
+ALTER table `newfi_schema`.`customerdetails` 
+   ADD COLUMN `equifax_score` int(5) NULL AFTER `customerspousedetails`, 
+   ADD COLUMN `transunion_score` int(5) NULL AFTER `equifax_score`, 
+   ADD COLUMN `experian_score` int(5) NULL AFTER `transunion_score`
+   
+   
+   
+   
+alter table `newfi_schema`.`customerspousedetails` 
+   ADD COLUMN `equifax_score` int(5) NULL AFTER `ss_disability_income`, 
+   ADD COLUMN `transunion_score` int(5) NULL AFTER `equifax_score`, 
+   ADD COLUMN `experian_score` int(5) NULL AFTER `transunion_scoreADD
