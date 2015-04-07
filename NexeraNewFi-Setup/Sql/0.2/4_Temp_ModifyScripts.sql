a791d163fc940cb503808910c283bdae5aa8dba5
rajeswarid <rajeswari@raremile.com>
2015-04-07 11:29:46 +0530

    Secondary Email of CUstomer Fix

--- NexeraNewFi-Setup/Sql/0.2/4_Temp_ModifyScripts.sql
+++ NexeraNewFi-Setup/Sql/0.2/4_Temp_ModifyScripts.sql
@@ -1,2 +1,5 @@
 #Please include your name and time the file was modified here, and write your alter staetments
 #Also Inform Anoop when you create a new table or alter a column, so that the dump can be taken accordingly
+
+#Rajeswari Added on 7 April.
+UPDATE `newfi_schema`.`workflowitemmaster` SET `on_success`='28' WHERE `id`='1';
