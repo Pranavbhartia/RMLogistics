708c33b96734cfcb75483a66d10eac6f387df415
akashbhatiararemile <akash@raremile.com>
2015-04-07 21:00:23 +0530

    added alter statement

--- NexeraNewFi-Setup/Sql/0.2/4_Temp_ModifyScripts.sql
+++ NexeraNewFi-Setup/Sql/0.2/4_Temp_ModifyScripts.sql
@@ -1,2 +1,7 @@
 #Please include your name and time the file was modified here, and write your alter staetments
 #Also Inform Anoop when you create a new table or alter a column, so that the dump can be taken accordingly
+
+
+#Added By akash on 07 April
+Alter TABLE newfi_schema.uploadedfileslist
+ADD COLUMN is_miscellaneous TINYINT(1) DEFAULT 1;
