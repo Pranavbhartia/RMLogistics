0cdc9b5b27a1dd245d7937c2369245a835ed3fab
rajeswarid <rajeswari@raremile.com>
2015-04-07 11:44:33 +0530

    Script cahnges

--- NexeraNewFi-Setup/Sql/0.2/4_Temp_ModifyScripts.sql
+++ NexeraNewFi-Setup/Sql/0.2/4_Temp_ModifyScripts.sql
@@ -3,3 +3,8 @@
 
 #Rajeswari Added on 7 April.
 UPDATE `newfi_schema`.`workflowitemmaster` SET `on_success`='28' WHERE `id`='1';
+
+#Utsav on 7 April
+ALTER TABLE `newfi_schema`.`internaluserdetails` 
+   ADD COLUMN `lqb_username` varchar(255) NULL after `user_role`, 
+   ADD COLUMN `lqb_password` varchar(255) NULL after `lqb_username`;
