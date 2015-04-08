0f1c3b1c7981eb444476d06165deadd2cde82363
utsav09211 <utsav.sharma@raremile.com>
2015-04-08 19:20:29 +0530

    Brain Tree Invoked Through Batch Process

--- NewFiWebApp/NewfiCore/src/main/java/com/nexera/core/service/TransactionService.java
+++ NewFiWebApp/NewfiCore/src/main/java/com/nexera/core/service/TransactionService.java
@@ -1,8 +1,14 @@
 package com.nexera.core.service;
 
+import java.util.List;
+
+import com.nexera.common.entity.Loan;
 import com.nexera.common.entity.LoanApplicationFee;
+import com.nexera.common.entity.TransactionDetails;
 import com.nexera.common.vo.LoanVO;
 
 public interface TransactionService {
 	public LoanApplicationFee findByLoan(LoanVO loanVo);
+
+	public List<TransactionDetails> getActiveTransactionsByLoan(Loan loan);
 }
