307226f85afec9c19a2e5a5a38e2a096f5c06cdf
Anoop <anoop@raremile.com>
2015-04-11 01:54:36 +0530

    Default loan manager assignment

--- NewFiWebApp/NewfiCore/src/main/java/com/nexera/core/service/impl/UserProfileServiceImpl.java
+++ NewFiWebApp/NewfiCore/src/main/java/com/nexera/core/service/impl/UserProfileServiceImpl.java
@@ -1095,7 +1095,7 @@ public class UserProfileServiceImpl implements UserProfileService,
 	public void addDefaultLM(UserVO userVO) {
 		// Get loan manager Id
 		;
-		userProfileDao.updateLMID(userVO.getRealtorDetail().getId(), utils
-		        .getLoggedInUser().getId());
+		userProfileDao.updateLMID(userVO.getId(), utils.getLoggedInUser()
+		        .getId());
 	}
 }
