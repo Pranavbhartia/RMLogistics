a4d4b655561c1421043375e4d25642364a5df15a
Anoop <anoop@raremile.com>
2015-04-10 21:44:30 +0530

    Added method to add default loan manager to realtor

--- NewFiWebApp/NewfiCore/src/test/java/com/nexera/test/service/USStateLookupTest.java
+++ NewFiWebApp/NewfiCore/src/test/java/com/nexera/test/service/USStateLookupTest.java
@@ -6,6 +6,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
+import com.nexera.common.dao.UserProfileDao;
 import com.nexera.common.exception.NonFatalException;
 import com.nexera.core.helper.USStateLookUp;
 import com.nexera.core.helper.USZipCodeLookup;
@@ -16,25 +17,33 @@ public class USStateLookupTest {
 
 	@Autowired
 	USStateLookUp usStateLookup;
-	
+
 	@Autowired
 	USZipCodeLookup usZipLookup;
-	
-	@Test
+
+	@Autowired
+	UserProfileDao dao;
+
+	// @Test
 	public void test() {
-		
+
 		/*
-		 * TODO: Uncomment if you want to populate the state and zip codes in ur local DB. It takes a while though
+		 * TODO: Uncomment if you want to populate the state and zip codes in ur
+		 * local DB. It takes a while though
 		 */
-		
+
 		try {
-			
+
 			usStateLookup.saveStatesInDB();
 			usZipLookup.saveZipCodeInDB();
-        } catch (NonFatalException e) {
-	        // TODO Auto-generated catch block
-	        e.printStackTrace();
-        }
+		} catch (NonFatalException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
 	}
 
+	@Test
+	public void testUserProfile() {
+		dao.updateLMID(1, 25);
+	}
 }
