2cf0a7b3b2bb0a5a42537fd72884cd0b8266ff9f
shashanktripathi01 <shashank.tripathi01@gmail.com>
2015-04-13 22:36:30 -0700

    Fixing issues related to Teaser Rate Page and employment income

--- NewFiWebApp/NewfiWeb/src/main/java/com/nexera/web/rest/util/TeaserRateHandler.java
+++ NewFiWebApp/NewfiWeb/src/main/java/com/nexera/web/rest/util/TeaserRateHandler.java
@@ -1,5 +1,6 @@
 package com.nexera.web.rest.util;
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
 
 import org.slf4j.Logger;
@@ -26,7 +27,8 @@ public class TeaserRateHandler extends DefaultHandler {
     private TeaserRateResponseVO teaserRateVo = new TeaserRateResponseVO();
     private ArrayList<LqbTeaserRateVo> rateVoList = new  ArrayList<LqbTeaserRateVo>() ;
     String tempVal = "";
-    
+    String totalClosingCostVal="";
+    HashMap<String,String> tempMap = new HashMap<String,String>();
     //getter method for TeaserRateResponseVO list
     public List<TeaserRateResponseVO> getTeaserRateList() {
         return teaserRateList;
@@ -36,7 +38,7 @@ public class TeaserRateHandler extends DefaultHandler {
     public void startElement(String uri, String localName, String qName,
     		Attributes attributes) throws SAXException {
     	
-    	LOG.info("StartElement  qName # "+qName);
+    	//LOG.info("StartElement  qName # "+qName);
     	
     		if(qName.equalsIgnoreCase("Program")) {
     			rateVoList = new  ArrayList<LqbTeaserRateVo>() ;
@@ -47,19 +49,102 @@ public class TeaserRateHandler extends DefaultHandler {
     		
     		if(qName.equalsIgnoreCase("RateOption")) {
     			LqbTeaserRateVo rateVo = new LqbTeaserRateVo();
-    			rateVo.setTeaserRate(attributes.getValue("Rate"));
-    			rateVo.setClosingCost(attributes.getValue("TotalClosingCost"));
-    			rateVo.setAPR(attributes.getValue("APR"));
-    			rateVo.setlLpTemplateId(attributes.getValue("lLpTemplateId"));
-    			rateVo.setPayment(attributes.getValue("Payment"));
-    			rateVo.setPoint(attributes.getValue("Point"));
+    				
+    			
+    			
+    			if(attributes.getValue("TotalClosingCost").toString().contains("(")){
+    				//totalClosingCostVal= attributes.getValue("Rate");
+    				tempMap.put("Rate",attributes.getValue("Rate"));
+    				tempMap.put("APR",attributes.getValue("APR"));
+    				tempMap.put("lLpTemplateId",attributes.getValue("lLpTemplateId"));
+    				tempMap.put("Payment",attributes.getValue("Payment"));
+    				tempMap.put("Point",attributes.getValue("Point"));
+    				
+    				
+    				
+    				
+    				LOG.info("TotalClosingCost contains ( "+totalClosingCostVal);
+    			}
+    			else{
+    				LOG.info("TotalClosingCost does not contains (");
+    				//if(!"".equalsIgnoreCase(totalClosingCostVal))
+    				if(tempMap !=null && tempMap.size() !=0)
+    				{
+    				LOG.info("TotalClosingCost is not equal to space hence setting cost as 0");
+    				LqbTeaserRateVo tempRateVo = new LqbTeaserRateVo();
+    				tempRateVo.setClosingCost("0");
+    				tempRateVo.setTeaserRate(tempMap.get("Rate"));
+    				tempRateVo.setAPR(tempMap.get("APR"));
+    				tempRateVo.setlLpTemplateId(tempMap.get("lLpTemplateId"));
+    				tempRateVo.setPayment(tempMap.get("Payment"));
+    				tempRateVo.setPoint(tempMap.get("Point"));
+    				
+    				LOG.info("Setting rate VO list tempRateVo values is "+tempRateVo.getClosingCost()+"  "+tempRateVo.getTeaserRate());
+    				
+    				rateVoList.add(tempRateVo);
+    					
+    				tempMap.clear();
+    				
+    					rateVo.setClosingCost(attributes.getValue("TotalClosingCost"));
+    					rateVo.setTeaserRate(attributes.getValue("Rate"));
+    					rateVo.setAPR(attributes.getValue("APR"));
+    	    			rateVo.setlLpTemplateId(attributes.getValue("lLpTemplateId"));
+    	    			rateVo.setPayment(attributes.getValue("Payment"));
+    	    			rateVo.setPoint(attributes.getValue("Point"));
+    					LOG.info("Setting rate VO list rateVo values is "+rateVo.getClosingCost()+"  "+rateVo.getTeaserRate());
+    					rateVoList.add(rateVo);
+    					totalClosingCostVal="";
+    					
+    					
+    					
+    					
+    				}else{
+    					LOG.info("Inside else"); 
+    				rateVo.setClosingCost(attributes.getValue("TotalClosingCost"));
+    				rateVo.setTeaserRate(attributes.getValue("Rate"));
+    				rateVo.setAPR(attributes.getValue("APR"));
+        			rateVo.setlLpTemplateId(attributes.getValue("lLpTemplateId"));
+        			rateVo.setPayment(attributes.getValue("Payment"));
+        			rateVo.setPoint(attributes.getValue("Point"));
+    				if(rateVoList.size()<=5)
+        			rateVoList.add(rateVo);
+    				}
+    			}
     			
     			
     			
-    			rateVoList.add(rateVo);
+    			
+    			
+    			
+    		
+    			
+    			
+    			
     			
     		}
     	}
+    
+    
+    
+    
+    
+    
+
+	
+	
+	
+	
+	
+
+    
+    
+    
+    
+    
+    
+    
+    
+    
     
     
     
@@ -67,7 +152,7 @@ public class TeaserRateHandler extends DefaultHandler {
     @Override
     public void endElement(String uri, String localName,
     		String qName) throws SAXException {
-    	LOG.info("end Element  qName # "+qName);
+    	//LOG.info("end Element  qName # "+qName);
     		if(qName.equalsIgnoreCase("Program")) {
     			//add it to the list
     			teaserRateVo.setRateVO(rateVoList);
