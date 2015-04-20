3b6cfa40d2cd174e2bd92df9cc2d368a3267e2d4
rajeswarid <rajeswari@raremile.com>
2015-04-20 19:05:09 +0530

    Gateway Service Revert

--- NewFiWebApp/NewfiCore/src/main/java/com/nexera/core/service/impl/BraintreePaymentGatewayServiceImpl.java
+++ NewFiWebApp/NewfiCore/src/main/java/com/nexera/core/service/impl/BraintreePaymentGatewayServiceImpl.java
@@ -390,7 +390,7 @@ public class BraintreePaymentGatewayServiceImpl implements
 		}
 
 		// Now we check the different possible cases of the transaction status
-		if (transaction.getStatus() == Transaction.Status.SUBMITTED_FOR_SETTLEMENT) {
+		if (transaction.getStatus() == Transaction.Status.SETTLED) {
 
 			User sysAdmin = new User();
 			sysAdmin.setId(CommonConstants.SYSTEM_ADMIN_ID);
