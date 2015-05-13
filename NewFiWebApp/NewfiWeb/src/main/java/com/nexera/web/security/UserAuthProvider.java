f6e9e03447822cfd1aa3372a42fd0b1f29ef7143
rajeswarid <rajeswari@raremile.com>
2015-05-13 17:29:51 +0530

    Resend Registration Link : Fix

--- NewFiWebApp/NewfiWeb/src/main/java/com/nexera/web/security/UserAuthProvider.java
+++ NewFiWebApp/NewfiWeb/src/main/java/com/nexera/web/security/UserAuthProvider.java
@@ -14,12 +14,12 @@ import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.security.web.authentication.WebAuthenticationDetails;
 
 import com.nexera.common.commons.DisplayMessageConstants;
 import com.nexera.common.entity.User;
 import com.nexera.common.exception.InvalidInputException;
 import com.nexera.common.exception.NoRecordsFetchedException;
-import com.nexera.common.vo.UserVO;
 import com.nexera.core.authentication.AuthenticationService;
 import com.nexera.core.helper.MessageServiceHelper;
 import com.nexera.core.service.UserProfileService;
@@ -34,24 +34,33 @@ public class UserAuthProvider extends DaoAuthenticationProvider {
 
 	@Autowired
 	private MessageServiceHelper messageServiceHelper;
-	
+
 	@Autowired
 	private UserProfileService userProfileService;
-
 
 	@Override
 	public Authentication authenticate(Authentication authentication) {
 		LOG.info("Inside authenticate Method of UserAuthProvider");
 		String username = authentication.getName().split(":")[0];
+
 		String offSet = authentication.getName().split(":")[1];
+
+		String[] tokens = authentication.getName().split(":");
+		boolean isShopper = false;
+		if (tokens.length == 3
+		        && authentication.getName().split(":")[2]
+		                .equals(DisplayMessageConstants.IS_SHOPPER)) {
+			isShopper = true;
+		}
 		String password = authentication.getCredentials().toString();
+
 		User user = null;
 		try {
 			LOG.debug("Validating the form parameters");
 			validateLoginFormParameters(username, password);
 			User userFromTable = userProfileService.findUserByMail(username);
-			if (userFromTable != null && userFromTable.getLastLoginDate()==null)
-			{
+			if (userFromTable != null
+			        && userFromTable.getLastLoginDate() == null && !isShopper) {
 				throw new DisabledException("First time login");
 			}
 			user = authenticationService.getUserWithLoginName(username,
@@ -63,7 +72,7 @@ public class UserAuthProvider extends DaoAuthenticationProvider {
 			} else if (user.getStatus() == 0) {
 				throw new DisabledException(
 				        DisplayMessageConstants.USER_INACTIVE);
-			} else if (user.getLastLoginDate() == null) {
+			} else if (user.getLastLoginDate() == null && !isShopper) {
 				throw new DisabledException("First time login");
 			}
 			authenticationService.validateUser(user, password);
@@ -78,7 +87,7 @@ public class UserAuthProvider extends DaoAuthenticationProvider {
 				        + user.getEmailId());
 				return auth;
 			}
-		} catch (NoRecordsFetchedException e) {			
+		} catch (NoRecordsFetchedException e) {
 			LOG.error("User not found in the system: " + e.getMessage());
 			throw new UsernameNotFoundException(
 			        "Please enter valid credentials");
