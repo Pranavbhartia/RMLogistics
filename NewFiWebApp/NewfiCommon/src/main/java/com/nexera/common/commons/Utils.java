b57cc96679975de2a012d85e8fae664ed48bb79f
Anoop <anoop@raremile.com>
2015-05-14 17:13:11 +0530

    Fixes for safari image cropping

--- NewFiWebApp/NewfiCommon/src/main/java/com/nexera/common/commons/Utils.java
+++ NewFiWebApp/NewfiCommon/src/main/java/com/nexera/common/commons/Utils.java
@@ -5,6 +5,7 @@ import java.awt.Graphics2D;
 import java.awt.Rectangle;
 import java.awt.image.BufferedImage;
 import java.awt.image.RasterFormatException;
+import java.io.File;
 import java.io.IOException;
 import java.io.StringWriter;
 import java.math.BigDecimal;
@@ -17,6 +18,8 @@ import java.util.Date;
 import java.util.List;
 import java.util.Map;
 import java.util.TimeZone;
+
+import javax.imageio.ImageIO;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -334,9 +337,8 @@ public class Utils {
 
 		return null;
 	}
-	
-	public String getTimeInPST()
-	{
+
+	public String getTimeInPST() {
 		DateFormat df = DateFormat.getDateTimeInstance(DateFormat.FULL,
 		        DateFormat.FULL);
 		df.setTimeZone(TimeZone.getTimeZone("PST"));
@@ -367,6 +369,29 @@ public class Utils {
 		g.dispose();
 
 		return resizedImage;
+	}
+
+	public void resize(String inputImagePath, String outputImagePath,
+	        int scaledWidth, int scaledHeight) throws IOException {
+		// reads input image
+		File inputFile = new File(inputImagePath);
+		BufferedImage inputImage = ImageIO.read(inputFile);
+
+		// creates output image
+		BufferedImage outputImage = new BufferedImage(scaledWidth,
+		        scaledHeight, inputImage.getType());
+
+		// scales the input image to the output image
+		Graphics2D g2d = outputImage.createGraphics();
+		g2d.drawImage(inputImage, 0, 0, scaledWidth, scaledHeight, null);
+		g2d.dispose();
+
+		// extracts extension of output file
+		String formatName = outputImagePath.substring(outputImagePath
+		        .lastIndexOf(".") + 1);
+
+		// writes to output file
+		ImageIO.write(outputImage, formatName, new File(outputImagePath));
 	}
 
 	public BufferedImage cropMyImage(BufferedImage img, int cropWidth,
