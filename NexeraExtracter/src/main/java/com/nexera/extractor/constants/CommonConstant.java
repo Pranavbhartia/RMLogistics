12002d293dfde4944ad93fdf2ea9a07d17d4ddad
ankit-raremile <ankit.srivastava@raremile.com>
2015-05-22 17:40:36 +0530

    adding urls

--- NexeraExtracter/src/main/java/com/nexera/extractor/constants/CommonConstant.java
+++ NexeraExtracter/src/main/java/com/nexera/extractor/constants/CommonConstant.java
@@ -8,4 +8,7 @@ public class CommonConstant {
 	public final String  FOLDER_PATH = "C:\\apps\\LQB\\Price\\";
 	public final Integer COLUMN_IN_SINGLE_ROW = 3;
 	public final String DEFAULT_FILE_NAME = "blustreamRS.xls";
+	public static final String PATH_FOR_ORIGINAL_TEMPELATE = "C:\\apps\\LQB\\blustreamRS.xls";
+	public static final String TEMP_PATH_FOR_COPIED_TEMPELATE = "C:\\apps\\LQB\\temp";
+	public static final String COPIED_TEMPELATE = "C:\\apps\\LQB\\temp\\blustreamRS.xls";
 }
