f5753561c3e572fb0f9b135c46762ec08889b689
utsav09211 <utsav.sharma@raremile.com>
2015-06-04 19:13:52 +0530

    Temp Script Changed

--- NexeraNewFi-Setup/Sql/0.2/4_Temp_ModifyScripts.sql
+++ NexeraNewFi-Setup/Sql/0.2/4_Temp_ModifyScripts.sql
@@ -11,3 +11,7 @@ ADD COLUMN `lqb_expiry_time` DATETIME NULL AFTER `lqb_auth_token`;
 
 ALTER TABLE `newfi_schema`.`internaluserdetails` 
    change `lqb_expiry_time` `lqb_expiry_time` BIGINT NULL;
+
+   
+ALTER TABLE `newfi_schema`.`uploadedfileslist` 
+   change `assigned_by` `assigned_by` int(11) NULL;
