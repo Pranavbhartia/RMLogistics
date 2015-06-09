0a4034a9b1c9e40efea00ca2cd4be2c332e60546
Anoop <anoop@raremile.com>
2015-06-09 12:05:51 +0530

    Sending text/html as part of sendgrid email

--- NewFiWebApp/NewfiCore/src/main/java/com/nexera/core/service/impl/SendGridEmailServiceImpl.java
+++ NewFiWebApp/NewfiCore/src/main/java/com/nexera/core/service/impl/SendGridEmailServiceImpl.java
@@ -205,11 +205,14 @@ public class SendGridEmailServiceImpl implements SendGridEmailService,
 		email.setFrom(emailEntity.getSenderEmailId());
 		email.setFromName(emailEntity.getSenderName());
 		email.setSubject(emailEntity.getSubject());
+
 		if (emailEntity.getBody() == null || emailEntity.getBody().isEmpty()) {
 
 			email.setText(CommonConstants.EMAIL_EMPTY_FOOTER);
+			email.setHtml(CommonConstants.EMAIL_EMPTY_FOOTER);
 		} else {
 			email.setText(emailEntity.getBody());
+			email.setHtml(emailEntity.getBody());
 		}
 		if (emailEntity.getAttachmentStream() != null) {
 			try {
