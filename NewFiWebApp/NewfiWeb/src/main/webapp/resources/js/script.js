8262a8753ad290b17af681ca80149b39a57868c0
charujoshi <charuindia12@gmail.com>
2015-07-09 16:28:40 +0530

    removed contact your LM button and send pre qualification

--- NewFiWebApp/NewfiWeb/src/main/webapp/resources/js/script.js
+++ NewFiWebApp/NewfiWeb/src/main/webapp/resources/js/script.js
@@ -2764,7 +2764,7 @@ function sendPreQualificationLetter(){
 }
 
 
-function getRequestRateLockStatus(element,isViaEngagementPath){
+function getRequestRateLockStatus(element){
     if(!appUserDetails.loan.isRateLocked){
         var loanId=appUserDetails.loan.id;
         ajaxRequest("rest/loan/"+loanId+"/rateCheck","GET","json",undefined,function(response){
@@ -2784,24 +2784,22 @@ function getRequestRateLockStatus(element,isViaEngagementPath){
                     	element.html( "Rate Lock Requested").unbind( "click").addClass("rateLockRequested");
                     }
                 }else{
-                	if(isViaEngagementPath){
-                		element.addClass("rate-btn");
-                        element.html("Contact Your Loan Advisor").on('click',function(){
-                         //changeLeftPanel(1);
-                        	
-                        	if(newfiObject.user.userRole.roleCd!="REALTOR"){
-                        	
-    	                        if(typeof(selectedUserDetail)!=='undefined'){
-    	                            window.location.hash="#loan/"+selectedUserDetail.loanID+"/progress"
-    	                        }else{
-    	                            window.location.hash="#myTeam";  
-    	                        }
-                        	}else{
-                        		showToastMessage("Please contact your customer");
-                        	}
-                        });
-                	}
-                	
+                    $(element).remove();
+            		/*element.addClass("rate-btn");
+                    element.html("Contact Your Loan Advisor").on('click',function(){
+                     //changeLeftPanel(1);
+                    	
+                    	if(newfiObject.user.userRole.roleCd!="REALTOR"){
+                    	
+	                        if(typeof(selectedUserDetail)!=='undefined'){
+	                            window.location.hash="#loan/"+selectedUserDetail.loanID+"/progress"
+	                        }else{
+	                            window.location.hash="#myTeam";  
+	                        }
+                    	}else{
+                    		showToastMessage("Please contact your customer");
+                    	}
+                    });*/
                 }
             }
         });
