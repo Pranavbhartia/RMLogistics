dbaa5bb02c19482c8079586cf74ed6f90d47e7c0
unknown <himanshu.matta30@gmail.com>
2015-10-01 18:59:53 +0530

    funded should go to archieve

--- NewFiWebApp/NewfiWorkFlow/src/main/java/com/nexera/newfi/workflow/tasks/ClosureStatusManager.java
+++ NewFiWebApp/NewfiWorkFlow/src/main/java/com/nexera/newfi/workflow/tasks/ClosureStatusManager.java
@@ -5,6 +5,10 @@ import java.util.HashMap;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
+import com.nexera.common.commons.WorkflowDisplayConstants;
+import com.nexera.common.entity.LoanProgressStatusMaster;
+import com.nexera.common.enums.LOSLoanStatus;
+import com.nexera.common.enums.LoanProgressStatusMasterEnum;
 import com.nexera.core.service.LoanService;
 import com.nexera.workflow.enums.WorkItemStatus;
 import com.nexera.workflow.task.IWorkflowTaskExecutor;
@@ -18,6 +22,14 @@ public class ClosureStatusManager extends NexeraWorkflowTask implements
 
 	@Override
 	public String execute(HashMap<String, Object> objectMap) {
+		String status = objectMap.get(
+		        WorkflowDisplayConstants.WORKITEM_STATUS_KEY_NAME).toString();
+		if (status.equals(String.valueOf(LOSLoanStatus.LQB_STATUS_FUNDED
+		        .getLosStatusID()))) {
+			int loanId = Integer.parseInt(objectMap.get(
+			          WorkflowDisplayConstants.LOAN_ID_KEY_NAME).toString());
+			loanService.saveLoanProgress(loanId, new LoanProgressStatusMaster(LoanProgressStatusMasterEnum.SMCLOSED));
+		}
 		String returnStatus = WorkItemStatus.COMPLETED.getStatus();
 		return returnStatus;
 	}
