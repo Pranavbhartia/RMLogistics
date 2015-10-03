1b08069e0574c8e1f2eb31ee22aad46845c9f51d
Ranjitha.R <ranjitha.r@raremile.com>
2015-10-03 11:14:08 +0530

    Fedric Rate Changes and logs

--- NexeraExtracter/src/main/java/com/nexera/extractor/entity/FileExtractorResposne.java
+++ NexeraExtracter/src/main/java/com/nexera/extractor/entity/FileExtractorResposne.java
@@ -1,5 +1,6 @@
 package com.nexera.extractor.entity;
 
+import java.util.Date;
 import java.util.List;
 import java.util.Map;
 
@@ -7,6 +8,26 @@ public class FileExtractorResposne {
 
 	private Map<String, List<UIEntity>> fileDetailList;
 	private Long folderTimeStamp;
+	private Date folderTSDtFormat;
+	private String folderTSDsFormat;
+	public Date getFolderTSDtFormat() {
+		return folderTSDtFormat;
+	}
+	public void setFolderTSDtFormat(Date folderTSDtFormat) {
+		this.folderTSDtFormat = folderTSDtFormat;
+	}
+	public String getFolderTSDsFormat() {
+		return folderTSDsFormat;
+	}
+	public void setFolderTSDsFormat(String folderTSDsFormat) {
+		this.folderTSDsFormat = folderTSDsFormat;
+	}
+	public Date getCurrentDate() {
+		return folderTSDtFormat;
+	}
+	public void setCurrentDate(Date currentDate) {
+		this.folderTSDtFormat = currentDate;
+	}
 	public Map<String, List<UIEntity>> getFileDetailList() {
 		return fileDetailList;
 	}
