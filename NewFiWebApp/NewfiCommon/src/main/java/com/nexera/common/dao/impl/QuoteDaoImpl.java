05c90aba82f01b8e890cd58b427476e95c6e9757
unknown <himanshu.matta30@gmail.com>
2015-10-22 15:32:21 +0530

    added foreign key reference between quotedetails and loan table

--- NewFiWebApp/NewfiCommon/src/main/java/com/nexera/common/dao/impl/QuoteDaoImpl.java
+++ NewFiWebApp/NewfiCommon/src/main/java/com/nexera/common/dao/impl/QuoteDaoImpl.java
@@ -1,116 +1,109 @@
 package com.nexera.common.dao.impl;
 
-import java.util.Date;
 import java.util.Iterator;
 import java.util.List;
 
-import org.hibernate.Criteria;
 import org.hibernate.Query;
 import org.hibernate.SQLQuery;
 import org.hibernate.Session;
-import org.hibernate.criterion.Restrictions;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Component;
 
 import com.nexera.common.compositekey.QuoteCompositeKey;
 import com.nexera.common.dao.QuoteDao;
-import com.nexera.common.entity.LoanAppForm;
-import com.nexera.common.entity.LoanTeam;
-import com.nexera.common.entity.LoanTypeMaster;
+import com.nexera.common.entity.Loan;
 import com.nexera.common.entity.QuoteDetails;
-import com.nexera.common.vo.QuoteDetailsVO;
 
 @Component
-public class QuoteDaoImpl extends GenericDaoImpl implements QuoteDao{
+public class QuoteDaoImpl extends GenericDaoImpl implements QuoteDao {
 
 	private static final Logger LOG = LoggerFactory
 	        .getLogger(QuoteDaoImpl.class);
-	
+
 	public boolean addQuoteDetails(QuoteDetails quoteDetais) {
 		boolean status = true;
-		try{
+		try {
 			this.saveOrUpdate(quoteDetais);
-		}
-		catch(Exception e){
-			LOG.error("Error in inserting/updating quote details: "+e);
+		} catch (Exception e) {
+			LOG.error("Error in inserting/updating quote details: " + e);
 			status = false;
-		}
-		finally{
+		} finally {
 			return status;
 		}
 
 	}
-	
-	public QuoteDetails getUserDetails(QuoteCompositeKey compKey){
-		QuoteDetails quoteDetails = (QuoteDetails)this.load(QuoteDetails.class, compKey);
+
+	public QuoteDetails getUserDetails(QuoteCompositeKey compKey) {
+		QuoteDetails quoteDetails = (QuoteDetails) this.load(QuoteDetails.class,
+		        compKey);
 		return quoteDetails;
 	}
-	
-	public QuoteDetails findQuoteDetailsById(String id){
+
+	public QuoteDetails findQuoteDetailsById(String id) {
 		QuoteDetails quoteDetails = null;
-		try{
-				Session session = sessionFactory.getCurrentSession();
-				SQLQuery qry = session.createSQLQuery("select * from quotedetails where id = :id");
-				qry.addEntity(QuoteDetails.class);
-				qry.setParameter("id", id);
-				// Here PRODUCTS is the table in the database...
-				List<QuoteDetails> list = qry.list();
-				for(QuoteDetails quote : list){
-					quoteDetails = quote;
-				}
-				
+		try {
+			Session session = sessionFactory.getCurrentSession();
+			SQLQuery qry = session.createSQLQuery(
+			        "select * from quotedetails where id = :id");
+			qry.addEntity(QuoteDetails.class);
+			qry.setParameter("id", id);
+			// Here PRODUCTS is the table in the database...
+			List<QuoteDetails> list = qry.list();
+			for (QuoteDetails quote : list) {
+				quoteDetails = quote;
+			}
+
+		} catch (Exception e) {
+			LOG.error(
+			        "Error in fetching quoteDetails on the basis of id: " + e);
 		}
-		catch(Exception e){
-			LOG.error("Error in fetching quoteDetails on the basis of id: "+e);
-		}
-				return quoteDetails;
+		return quoteDetails;
 	}
-	
-	public String  getUniqueIdFromQuoteDetails(QuoteCompositeKey compKey){
-		String id = null ;
-		try{
-				Session session = sessionFactory.getCurrentSession();
-				SQLQuery qry = session.createSQLQuery("select id from quotedetails where prospect_username =  :username and internal_user_id = :internalUserId");
-				qry.setParameter("username", compKey.getUserName());
-				qry.setParameter("internalUserId", compKey.getInternalUserId());
-				// Here PRODUCTS is the table in the database...
-				List list = qry.list();
-				Iterator it = list.iterator();
-				
-				if (it.hasNext())	{
-					id = ""+it.next();
-				}
+
+	public String getUniqueIdFromQuoteDetails(QuoteCompositeKey compKey) {
+		String id = null;
+		try {
+			Session session = sessionFactory.getCurrentSession();
+			SQLQuery qry = session.createSQLQuery(
+			        "select id from quotedetails where prospect_username =  :username and internal_user_id = :internalUserId");
+			qry.setParameter("username", compKey.getUserName());
+			qry.setParameter("internalUserId", compKey.getInternalUserId());
+			// Here PRODUCTS is the table in the database...
+			List list = qry.list();
+			Iterator it = list.iterator();
+
+			if (it.hasNext()) {
+				id = "" + it.next();
+			}
+		} catch (Exception e) {
+			LOG.error("Error in fetching id from quotedetail table: " + e);
 		}
-		catch(Exception e){
-			LOG.error("Error in fetching id from quotedetail table: "+e);
-		}
-				return id;
+		return id;
 	}
 
 	@Override
 	public void updateCreatedUser(QuoteCompositeKey compKey) {
-		
+
 		Session session = sessionFactory.getCurrentSession();
 		String hql = "UPDATE QuoteDetails quote set quote.isCreated = :isCreated WHERE quote.quoteCompositeKey = :quoteCompositeKey";
 		Query query = session.createQuery(hql);
 		query.setParameter("isCreated", true);
 		query.setParameter("quoteCompositeKey", compKey);
-		query.executeUpdate();	
+		query.executeUpdate();
 	}
-	
+
 	@Override
-	public void updateLoanId(QuoteCompositeKey compKey, Integer loanId) {
-		
+	public void updateLoanId(QuoteCompositeKey compKey, Loan loan) {
+
 		Session session = sessionFactory.getCurrentSession();
-		String hql = "UPDATE QuoteDetails quote set quote.loanId = :loanId WHERE quote.quoteCompositeKey = :quoteCompositeKey";
+		String hql = "UPDATE QuoteDetails quote set quote.loan = :loan WHERE quote.quoteCompositeKey = :quoteCompositeKey";
 		Query query = session.createQuery(hql);
-		query.setParameter("loanId", loanId);
+		query.setParameter("loan", loan);
 		query.setParameter("quoteCompositeKey", compKey);
-		query.executeUpdate();	
+		query.executeUpdate();
 	}
-	
-	
+
 	@Override
 	public void updateDeletedUser(QuoteCompositeKey compKey) {
 		Session session = sessionFactory.getCurrentSession();
@@ -118,7 +111,7 @@ public class QuoteDaoImpl extends GenericDaoImpl implements QuoteDao{
 		Query query = session.createQuery(hql);
 		query.setParameter("isDeleted", true);
 		query.setParameter("quoteCompositeKey", compKey);
-		query.executeUpdate();	
+		query.executeUpdate();
 	}
-	
+
 }
--- NewFiWebApp/NewfiCore/src/main/java/com/nexera/core/service/impl/QuoteServiceImpl.java
+++ NewFiWebApp/NewfiCore/src/main/java/com/nexera/core/service/impl/QuoteServiceImpl.java
@@ -2,8 +2,6 @@ package com.nexera.core.service.impl;
 
 import java.util.ArrayList;
 import java.util.Date;
-import java.util.HashMap;
-import java.util.Map;
 
 import org.json.JSONObject;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -14,12 +12,11 @@ import com.google.gson.Gson;
 import com.nexera.common.compositekey.QuoteCompositeKey;
 import com.nexera.common.dao.LoanDao;
 import com.nexera.common.dao.QuoteDao;
+import com.nexera.common.entity.Loan;
 import com.nexera.common.entity.PurchaseDetails;
 import com.nexera.common.entity.QuoteDetails;
 import com.nexera.common.vo.GeneratePdfVO;
-import com.nexera.common.vo.LoanAppFormVO;
 import com.nexera.common.vo.QuoteDetailsVO;
-import com.nexera.common.vo.UserVO;
 import com.nexera.common.vo.lqb.LqbTeaserRateVo;
 import com.nexera.common.vo.lqb.TeaserRateResponseVO;
 import com.nexera.common.vo.lqb.TeaserRateVO;
@@ -30,17 +27,17 @@ public class QuoteServiceImpl implements QuoteService {
 
 	@Autowired
 	private QuoteDao quoteDao;
-	
+
 	@Autowired
 	private LoanDao loanDao;
-	
+
 	@Transactional
 	public boolean addQuoteDetails(GeneratePdfVO generatePdfVO) {
 		String username = "";
 		Gson gson = new Gson();
 		QuoteDetails quoteDetails = new QuoteDetails();
 		QuoteCompositeKey quoteCompositeKey = new QuoteCompositeKey();
-		
+
 		quoteDetails.setProspectFirstName(generatePdfVO.getFirstName());
 		quoteDetails.setProspectLastName(generatePdfVO.getLastName());
 		quoteDetails.setEmailId(generatePdfVO.getEmailId());
@@ -48,79 +45,80 @@ public class QuoteServiceImpl implements QuoteService {
 		quoteDetails.setRateAndApr(generatePdfVO.getRateAndApr());
 		quoteDetails.setLoanProgram(generatePdfVO.getLoanProgram());
 		quoteDetails.setPdfUrl(generatePdfVO.getPdfUrl());
-		
-		if (generatePdfVO.getEmailId() != null){
-			username = generatePdfVO.getEmailId().split("@")[0];	
+
+		if (generatePdfVO.getEmailId() != null) {
+			username = generatePdfVO.getEmailId().split("@")[0];
 		}
-		
+
 		quoteCompositeKey.setUserName(username);
 		quoteCompositeKey.setInternalUserId(generatePdfVO.getUserId());
-		
+
 		quoteDetails.setQuoteCompositeKey(quoteCompositeKey);
-		
-		LqbTeaserRateVo lqbTeaserRateVo = generatePdfVO.getLqbTeaserRateUnderQuickQuote();
+
+		LqbTeaserRateVo lqbTeaserRateVo = generatePdfVO
+		        .getLqbTeaserRateUnderQuickQuote();
 		String lqbTeaserRateJson = gson.toJson(lqbTeaserRateVo);
 		quoteDetails.setLqbRateJson(lqbTeaserRateJson);
-		
-		TeaserRateVO teaserRateVO = generatePdfVO.getInputCustmerDetailUnderQuickQuote();
+
+		TeaserRateVO teaserRateVO = generatePdfVO
+		        .getInputCustmerDetailUnderQuickQuote();
 		String teaserRateJson = gson.toJson(teaserRateVO);
 		quoteDetails.setInputDetailsJson(teaserRateJson);
-		
+
 		quoteDetails.setCreatedDate(new Date(System.currentTimeMillis()));
-		
+
 		boolean status = quoteDao.addQuoteDetails(quoteDetails);
 		return status;
 	}
-	
+
 	@Transactional
-	public String  getUniqueIdFromQuoteDetails(QuoteCompositeKey compKey){
+	public String getUniqueIdFromQuoteDetails(QuoteCompositeKey compKey) {
 		return quoteDao.getUniqueIdFromQuoteDetails(compKey);
 	}
-	
+
 	@Transactional
-	public QuoteDetails findQuoteDetailsById(String id){
+	public QuoteDetails findQuoteDetailsById(String id) {
 		return quoteDao.findQuoteDetailsById(id);
 	}
-	
+
 	@Transactional
-	public QuoteDetails getUserDetails(QuoteCompositeKey quoteCompositeKey){
+	public QuoteDetails getUserDetails(QuoteCompositeKey quoteCompositeKey) {
 		QuoteDetails quoteDetails = quoteDao.getUserDetails(quoteCompositeKey);
 		return quoteDetails;
 	}
-	
-	
+
 	@Transactional
-	public boolean createNewUser(String userName, Integer internalUserId){
+	public boolean createNewUser(String userName, Integer internalUserId) {
 		QuoteCompositeKey key = new QuoteCompositeKey();
 		key.setInternalUserId(internalUserId);
 		key.setUserName(userName);
 		QuoteDetails quoteDetails = getUserDetails(key);
-		
+
 		return true;
 	}
 
 	@Transactional
-	public void updateCreatedUser(QuoteCompositeKey compKey){
+	public void updateCreatedUser(QuoteCompositeKey compKey) {
 		quoteDao.updateCreatedUser(compKey);
 	}
-	
+
 	@Transactional
-	public void updateLoanId(QuoteCompositeKey compKey, Integer loanId) {
-		quoteDao.updateLoanId(compKey, loanId);
+	public void updateLoanId(QuoteCompositeKey compKey, Loan loan) {
+		quoteDao.updateLoanId(compKey, loan);
 	}
-	
+
 	@Transactional
-	public void updateDeletedUser(QuoteCompositeKey compKey){
+	public void updateDeletedUser(QuoteCompositeKey compKey) {
 		quoteDao.updateDeletedUser(compKey);
 	}
-	
-	public String createTeaserRateData(QuoteDetails quoteDetails){
+
+	public String createTeaserRateData(QuoteDetails quoteDetails) {
 		Gson gson = new Gson();
 		String teaserRate = quoteDetails.getLqbRateJson();
 		return teaserRate;
 	}
-	
-	public String createRegistrationDetails(QuoteDetails quoteDetails){
+
+	public String createRegistrationDetails(QuoteDetails quoteDetails) {
 		Gson gson = new Gson();
 		JSONObject mainJson = new JSONObject();
 		JSONObject personalDetails = new JSONObject();
@@ -128,65 +126,71 @@ public class QuoteServiceImpl implements QuoteService {
 		JSONObject purchaseDetails = new JSONObject();
 		JSONObject propertyTypeMaster = new JSONObject();
 		JSONObject refinancedetails = new JSONObject();
-		
+
 		personalDetails.put("firstName", quoteDetails.getProspectFirstName());
 		personalDetails.put("lastName", quoteDetails.getProspectLastName());
 		personalDetails.put("emailId", quoteDetails.getEmailId());
-		
+
 		mainJson.put("user", personalDetails);
-		
-		TeaserRateVO teaserRateVO = gson.fromJson(quoteDetails.getInputDetailsJson(), TeaserRateVO.class);
-		
-		if(teaserRateVO.getLoanType().equals("REFCO")){
-			loanType.put("loanTypeCd","REF");
+
+		TeaserRateVO teaserRateVO = gson.fromJson(
+		        quoteDetails.getInputDetailsJson(), TeaserRateVO.class);
+
+		if (teaserRateVO.getLoanType().equals("REFCO")) {
+			loanType.put("loanTypeCd", "REF");
 			refinancedetails.put("cashTakeOut", teaserRateVO.getCashTakeOut());
+		} else {
+			loanType.put("loanTypeCd", teaserRateVO.getLoanType());
 		}
-		else{
-			loanType.put("loanTypeCd",teaserRateVO.getLoanType());
-		}
-		
+
 		mainJson.put("loanType", loanType);
-		
-		
-		if(teaserRateVO.getLoanType().equals("PUR")){
-			propertyTypeMaster.put("propertyTypeCd", teaserRateVO.getPropertyType());
-			propertyTypeMaster.put("residenceTypeCd", teaserRateVO.getResidenceType());
+
+		if (teaserRateVO.getLoanType().equals("PUR")) {
+			propertyTypeMaster.put("propertyTypeCd",
+			        teaserRateVO.getPropertyType());
+			propertyTypeMaster.put("residenceTypeCd",
+			        teaserRateVO.getResidenceType());
 			propertyTypeMaster.put("homeZipCode", teaserRateVO.getZipCode());
-			
-			mainJson.put("propertyTypeMaster", propertyTypeMaster); 
-			
+
+			mainJson.put("propertyTypeMaster", propertyTypeMaster);
+
 			PurchaseDetails purchase = teaserRateVO.getPurchaseDetails();
 			String purchaseJsonString = gson.toJson(purchase);
 			JSONObject purchaseJson = new JSONObject(purchaseJsonString);
-			
+
 			mainJson.put("purchaseDetails", purchaseJson);
-		}
-		else{
-			propertyTypeMaster.put("propertyTaxesPaid", teaserRateVO.getPropertyTaxesPaid());
-			propertyTypeMaster.put("propertyInsuranceCost", teaserRateVO.getAnnualHomeownersInsurance());
-			propertyTypeMaster.put("homeWorthToday", teaserRateVO.getHomeWorthToday());
+		} else {
+			propertyTypeMaster.put("propertyTaxesPaid",
+			        teaserRateVO.getPropertyTaxesPaid());
+			propertyTypeMaster.put("propertyInsuranceCost",
+			        teaserRateVO.getAnnualHomeownersInsurance());
+			propertyTypeMaster.put("homeWorthToday",
+			        teaserRateVO.getHomeWorthToday());
 			propertyTypeMaster.put("homeZipCode", teaserRateVO.getZipCode());
 			propertyTypeMaster.put("propTaxMonthlyOryearly", "Year");
 			propertyTypeMaster.put("propInsMonthlyOryearly", "Year");
-			propertyTypeMaster.put("propertyTypeCd", teaserRateVO.getPropertyType());
-			propertyTypeMaster.put("residenceTypeCd", teaserRateVO.getResidenceType());
-			
+			propertyTypeMaster.put("propertyTypeCd",
+			        teaserRateVO.getPropertyType());
+			propertyTypeMaster.put("residenceTypeCd",
+			        teaserRateVO.getResidenceType());
+
 			mainJson.put("propertyTypeMaster", propertyTypeMaster);
-			
-			refinancedetails.put("refinanceOption", teaserRateVO.getRefinanceOption());
-			refinancedetails.put("currentMortgageBalance", teaserRateVO.getCurrentMortgageBalance());
-			refinancedetails.put("includeTaxes", teaserRateVO.getPrivateincludeTaxes());
-			
+
+			refinancedetails.put("refinanceOption",
+			        teaserRateVO.getRefinanceOption());
+			refinancedetails.put("currentMortgageBalance",
+			        teaserRateVO.getCurrentMortgageBalance());
+			refinancedetails.put("includeTaxes",
+			        teaserRateVO.getPrivateincludeTaxes());
+
 			mainJson.put("refinancedetails", refinancedetails);
-			
-			
-			
+
 		}
 		return mainJson.toString();
-		
+
 	}
-	
-	public GeneratePdfVO convertToGeneratePdfVo(QuoteDetails quoteDetails){
+
+	public GeneratePdfVO convertToGeneratePdfVo(QuoteDetails quoteDetails) {
 		Gson gson = new Gson();
 		GeneratePdfVO generatePdfVO = new GeneratePdfVO();
 		generatePdfVO.setFirstName(quoteDetails.getProspectFirstName());
@@ -196,78 +200,82 @@ public class QuoteServiceImpl implements QuoteService {
 		generatePdfVO.setLoanProgram(quoteDetails.getLoanProgram());
 		generatePdfVO.setRateAndApr(quoteDetails.getRateAndApr());
 		generatePdfVO.setPhoneNo(quoteDetails.getPhoneNo());
-		generatePdfVO.setUserId(quoteDetails.getQuoteCompositeKey().getInternalUserId());
-		
-		
-		
-		TeaserRateVO teaserRateVO = gson.fromJson(quoteDetails.getInputDetailsJson(), TeaserRateVO.class);
+		generatePdfVO.setUserId(
+		        quoteDetails.getQuoteCompositeKey().getInternalUserId());
+
+		TeaserRateVO teaserRateVO = gson.fromJson(
+		        quoteDetails.getInputDetailsJson(), TeaserRateVO.class);
 		generatePdfVO.setInputCustmerDetailUnderQuickQuote(teaserRateVO);
-		
-		LqbTeaserRateVo lqbTeaserRateVo = gson.fromJson(quoteDetails.getLqbRateJson(), LqbTeaserRateVo.class);
+
+		LqbTeaserRateVo lqbTeaserRateVo = gson
+		        .fromJson(quoteDetails.getLqbRateJson(), LqbTeaserRateVo.class);
 		generatePdfVO.setLqbTeaserRateUnderQuickQuote(lqbTeaserRateVo);
-		
+
 		TeaserRateResponseVO teaserRateResponseVO = new TeaserRateResponseVO();
-		
-		
-		 String loanProgram = generatePdfVO.getLqbTeaserRateUnderQuickQuote().getYearData();
-	        if(loanProgram.equals("5") || loanProgram.equals("7")){
-	        	loanProgram = loanProgram+" - Year ARM";
-	     }
-	     else{
-	        	loanProgram = loanProgram+" - Year Fixed";
-	     }
-	        
-	     ArrayList<LqbTeaserRateVo> rateVO = new ArrayList<LqbTeaserRateVo>();
-	     rateVO.add(lqbTeaserRateVo);
-	     teaserRateResponseVO.setLoanDuration(loanProgram);
-	     teaserRateResponseVO.setRateVO(rateVO);
-	     
-	     generatePdfVO.setTeaserRateVO(teaserRateResponseVO);
-		
-	     return generatePdfVO;
+
+		String loanProgram = generatePdfVO.getLqbTeaserRateUnderQuickQuote()
+		        .getYearData();
+		if (loanProgram.equals("5") || loanProgram.equals("7")) {
+			loanProgram = loanProgram + " - Year ARM";
+		} else {
+			loanProgram = loanProgram + " - Year Fixed";
+		}
+
+		ArrayList<LqbTeaserRateVo> rateVO = new ArrayList<LqbTeaserRateVo>();
+		rateVO.add(lqbTeaserRateVo);
+		teaserRateResponseVO.setLoanDuration(loanProgram);
+		teaserRateResponseVO.setRateVO(rateVO);
+
+		generatePdfVO.setTeaserRateVO(teaserRateResponseVO);
+
+		return generatePdfVO;
 	}
-	
-	 public QuoteDetailsVO convertEntityToVO(QuoteDetails quoteDetails){
-		  QuoteDetailsVO quoteDetailsVO = new QuoteDetailsVO();
-		  quoteDetailsVO.setCreatedDate(quoteDetails.getCreatedDate());
-		  quoteDetailsVO.setEmailId(quoteDetails.getEmailId());
-		  quoteDetailsVO.setInputDetailsJson(quoteDetails.getInputDetailsJson());
-		  quoteDetailsVO.setInternalUserId(quoteDetails.getQuoteCompositeKey().getInternalUserId());
-		  quoteDetailsVO.setLqbRateJson(quoteDetails.getLqbRateJson());
-		  quoteDetailsVO.setPdfUrl(quoteDetails.getPdfUrl());
-		  quoteDetailsVO.setPhoneNo(quoteDetails.getPhoneNo());
-		  quoteDetailsVO.setProspectFirstName(quoteDetails.getProspectFirstName());
-		  quoteDetailsVO.setProspectLastName(quoteDetails.getProspectLastName());
-		  quoteDetailsVO.setIsCreated(quoteDetails.getIsCreated());
-		  quoteDetailsVO.setIsDeleted(quoteDetails.getIsDeleted());
-		  quoteDetailsVO.setProspectUsername(quoteDetails.getQuoteCompositeKey().getUserName());
-		  quoteDetailsVO.setId(quoteDetails.getId());
-		  return quoteDetailsVO;
-	 }
-	
-	 /**
-	  * @param quoteDetailsVO
-	  * @return
-	  */
-	 public  QuoteDetails convertVOToEntity(QuoteDetailsVO quoteDetailsVO){
-	  
-	  QuoteDetails quoteDetails = new QuoteDetails();
-	  quoteDetails.setCreatedDate(quoteDetailsVO.getCreatedDate());
-	  quoteDetails.setEmailId(quoteDetailsVO.getEmailId());
-	  quoteDetails.setInputDetailsJson(quoteDetailsVO.getInputDetailsJson());
-	  QuoteCompositeKey quoteCompositeKey = new QuoteCompositeKey();
-	  quoteCompositeKey.setInternalUserId(quoteDetailsVO.getInternalUserId());
-	  quoteCompositeKey.setUserName(quoteDetailsVO.getProspectUsername());
-	  quoteDetails.setQuoteCompositeKey(quoteCompositeKey);
-	  quoteDetails.setLqbRateJson(quoteDetailsVO.getLqbRateJson());
-	  quoteDetails.setPdfUrl(quoteDetailsVO.getPdfUrl());
-	  quoteDetails.setPhoneNo(quoteDetailsVO.getPhoneNo());
-	  quoteDetails.setProspectFirstName(quoteDetailsVO.getProspectFirstName());
-	  quoteDetails.setProspectLastName(quoteDetailsVO.getProspectLastName());
-	  quoteDetails.setIsCreated(quoteDetailsVO.getIsCreated());
-	  quoteDetails.setIsDeleted(quoteDetailsVO.getIsDeleted());  
-	  quoteDetails.setId(quoteDetailsVO.getId());
-	  return quoteDetails;
-	 }
-	 	 
+
+	public QuoteDetailsVO convertEntityToVO(QuoteDetails quoteDetails) {
+		QuoteDetailsVO quoteDetailsVO = new QuoteDetailsVO();
+		quoteDetailsVO.setCreatedDate(quoteDetails.getCreatedDate());
+		quoteDetailsVO.setEmailId(quoteDetails.getEmailId());
+		quoteDetailsVO.setInputDetailsJson(quoteDetails.getInputDetailsJson());
+		quoteDetailsVO.setInternalUserId(
+		        quoteDetails.getQuoteCompositeKey().getInternalUserId());
+		quoteDetailsVO.setLqbRateJson(quoteDetails.getLqbRateJson());
+		quoteDetailsVO.setPdfUrl(quoteDetails.getPdfUrl());
+		quoteDetailsVO.setPhoneNo(quoteDetails.getPhoneNo());
+		quoteDetailsVO
+		        .setProspectFirstName(quoteDetails.getProspectFirstName());
+		quoteDetailsVO.setProspectLastName(quoteDetails.getProspectLastName());
+		quoteDetailsVO.setIsCreated(quoteDetails.getIsCreated());
+		quoteDetailsVO.setIsDeleted(quoteDetails.getIsDeleted());
+		quoteDetailsVO.setProspectUsername(
+		        quoteDetails.getQuoteCompositeKey().getUserName());
+		quoteDetailsVO.setId(quoteDetails.getId());
+		return quoteDetailsVO;
+	}
+
+	/**
+	 * @param quoteDetailsVO
+	 * @return
+	 */
+	public QuoteDetails convertVOToEntity(QuoteDetailsVO quoteDetailsVO) {
+
+		QuoteDetails quoteDetails = new QuoteDetails();
+		quoteDetails.setCreatedDate(quoteDetailsVO.getCreatedDate());
+		quoteDetails.setEmailId(quoteDetailsVO.getEmailId());
+		quoteDetails.setInputDetailsJson(quoteDetailsVO.getInputDetailsJson());
+		QuoteCompositeKey quoteCompositeKey = new QuoteCompositeKey();
+		quoteCompositeKey.setInternalUserId(quoteDetailsVO.getInternalUserId());
+		quoteCompositeKey.setUserName(quoteDetailsVO.getProspectUsername());
+		quoteDetails.setQuoteCompositeKey(quoteCompositeKey);
+		quoteDetails.setLqbRateJson(quoteDetailsVO.getLqbRateJson());
+		quoteDetails.setPdfUrl(quoteDetailsVO.getPdfUrl());
+		quoteDetails.setPhoneNo(quoteDetailsVO.getPhoneNo());
+		quoteDetails
+		        .setProspectFirstName(quoteDetailsVO.getProspectFirstName());
+		quoteDetails.setProspectLastName(quoteDetailsVO.getProspectLastName());
+		quoteDetails.setIsCreated(quoteDetailsVO.getIsCreated());
+		quoteDetails.setIsDeleted(quoteDetailsVO.getIsDeleted());
+		quoteDetails.setId(quoteDetailsVO.getId());
+		return quoteDetails;
+	}
+
 }
